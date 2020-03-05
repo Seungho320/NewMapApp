89fc48e544c146562b452f4970c1f449519892ce
Seungho320 <splovetw@gmail.com>
2020-03-05 18:41:12 +0900

    Gpsmap

--- NewMapApp/ViewController.swift
+++ NewMapApp/ViewController.swift
@@ -7,14 +7,208 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate, UIGestureRecognizerDelegate {
+    
+    var mapViewType: UIButton!
+    
+    @IBOutlet var mapView: MKMapView!
+    var locManager: CLLocationManager!
+    @IBOutlet var longPressGesRec: UILongPressGestureRecognizer!
+    var pointAno: MKPointAnnotation = MKPointAnnotation()
+    
+    //拡大・縮小ボタンの設置
+    let goldenRatio = 1.618
+    @IBAction func clickZoomin(_ sender: Any) {
+        if (0.005 < mapView.region.span.latitudeDelta / goldenRatio) {
+            print(mapView.region.span.latitudeDelta.description)
+            var regionSpan:MKCoordinateSpan = MKCoordinateSpan()
+            regionSpan.latitudeDelta = mapView.region.span.latitudeDelta / goldenRatio
+            mapView.region.span = regionSpan
+            print(mapView.region.span.latitudeDelta.description)
+        }
+    }
+    @IBAction func clickZoomout(_ sender: Any) {
+            if (mapView.region.span.latitudeDelta * goldenRatio < 150.0) {
+                print(mapView.region.span.latitudeDelta.description)
+                var regionSpan:MKCoordinateSpan = MKCoordinateSpan()
+                regionSpan.latitudeDelta = mapView.region.span.latitudeDelta * goldenRatio
+        //            regionSpan.latitudeDelta = mapView.region.span.longitudeDelta * GoldenRatio
+                mapView.region.span = regionSpan
+                print(mapView.region.span.latitudeDelta.description)
+            }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // 地図の初期化
+               initMap()
+        
+        // 位置情報へのアクセスを要求する
+        locManager = CLLocationManager()
+        locManager.delegate = self
+        
+        // 位置情報の使用の許可を得る
+        locManager.requestWhenInUseAuthorization()
+        if CLLocationManager.locationServicesEnabled() {
+            switch CLLocationManager.authorizationStatus() {
+            case .authorizedWhenInUse:
+                // 座標の表示
+                locManager.startUpdatingLocation()
+                break
+            default:
+                break
+            }
+        }
     }
+    
+    // 地図の表示タイプを切り替える
+    @objc internal func mapViewTypeBtnThouchDown(_ sender: Any) {
+        switch mapView.mapType {
+        case .standard:         // 標準の地図
+            mapView.mapType = .satellite
+            break
+        case .satellite:        // 航空写真
+            mapView.mapType = .hybrid
+            break
+        case .hybrid:           // 標準の地図＋航空写真
+            mapView.mapType = .satelliteFlyover
+            break
+        case .satelliteFlyover: // 3D航空写真
+            mapView.mapType = .hybridFlyover
+            break
+        case .hybridFlyover:    // 3D標準の地図＋航空写真
+            mapView.mapType = .mutedStandard
+            break
+        case .mutedStandard:    // 地図よりもデータを強調
+            mapView.mapType = .standard
+            break
+        }
+        
+    }
+    
+    func updateCurrentPos(_ coordinate:CLLocationCoordinate2D) {
+        var region:MKCoordinateRegion = mapView.region
+        region.center = coordinate
+        mapView.setRegion(region,animated:true)
+    }
+    // CLLocationManagerのdelegate:　現在位置取得
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations:[CLLocation]) {
+        //updateCurrentPos((locations.last?.coordinate)!)
+        
+        // 現在位置とタップした位置の距離(m)を算出する
+        let distance = calcDistance(mapView.userLocation.coordinate, pointAno.coordinate)
 
+        if (0 != distance) {
+            // ピンに設定する文字列を生成する
+            var str:String = Int(distance).description
+            str = str + " m"
 
+            if pointAno.title != str {
+                // ピンまでの距離に変化があればtitleを更新する
+                pointAno.title = str
+                mapView.addAnnotation(pointAno)
+            }
+        }
+    }
+    
+    func initMap() {
+        // 縮尺を設定
+        var region:MKCoordinateRegion = mapView.region
+        region.span.latitudeDelta = 0.02
+        region.span.longitudeDelta = 0.02
+        mapView.setRegion(region,animated:true)
+
+        // 現在位置表示の有効化
+        mapView.showsUserLocation = true
+        // 現在位置設定（デバイスの動きとしてこの時の一回だけ中心位置が現在位置で更新される）
+        mapView.userTrackingMode = .follow
+        
+        // トラッキングボタン表示
+        let trakingBtn = MKUserTrackingButton(mapView: mapView)
+        trakingBtn.layer.backgroundColor = UIColor(white: 1, alpha: 0.7).cgColor
+        trakingBtn.frame = CGRect(x:40, y:730, width:40, height:40)
+        self.view.addSubview(trakingBtn)
+        // デバイスの画面サイズを取得する
+        let dispSize: CGSize = UIScreen.main.bounds.size
+        let width = Int(dispSize.width)
+        trakingBtn.frame = CGRect(x:width - 50, y:100, width:40, height:40)
+        
+        // スケールバーの表示
+        let scale = MKScaleView(mapView: mapView)
+        scale.frame.origin.x = 15
+        scale.frame.origin.y = 45
+        scale.legendAlignment = .leading
+        self.view.addSubview(scale)
+        
+        // コンパスの表示
+        let compass = MKCompassButton(mapView: mapView)
+        compass.compassVisibility = .visible
+        compass.frame = CGRect(x:width - 50, y: 150, width: 40, height: 40)
+        self.view.addSubview(compass)
+        // デフォルトのコンパスを非表示にする
+        mapView.showsCompass = false
+        
+        // 地図表示タイプを切り替えるボタンを配置する
+        mapViewType = UIButton(type: UIButton.ButtonType.detailDisclosure)
+        mapViewType.frame = CGRect(x:width - 50, y:60, width:40, height:40)
+        mapViewType.layer.backgroundColor = UIColor(white: 1, alpha: 0.8).cgColor // 背景色
+        mapViewType.layer.borderWidth = 0.5 // 枠線の幅
+        mapViewType.layer.borderColor = UIColor.blue.cgColor // 枠線の色
+        self.view.addSubview(mapViewType)
+        
+        mapViewType.addTarget(self, action: #selector(ViewController.mapViewTypeBtnThouchDown(_:)), for: .touchDown)
+    }
+    
+    // 2点間の距離(m)を算出する
+    func calcDistance(_ a: CLLocationCoordinate2D, _ b: CLLocationCoordinate2D) -> CLLocationDistance {
+        // CLLocationオブジェクトを生成
+        let aLoc: CLLocation = CLLocation(latitude: a.latitude, longitude: a.longitude)
+        let bLoc: CLLocation = CLLocation(latitude: b.latitude, longitude: b.longitude)
+        // CLLocationオブジェクトのdistanceで2点間の距離(m)を算出
+        let dist = bLoc.distance(from: aLoc)
+        return dist
+    }
+    
+    @IBAction func mapViewDidLongPress(_ sender: UILongPressGestureRecognizer) {
+        // ロングタップ開始
+        if sender.state == .began {
+            // ロングタップ開始時に古いピンを削除する
+            mapView.removeAnnotation(pointAno)
+        }
+        // ロングタップ終了（手を離した）
+        else if sender.state == .ended {
+            // タップした位置（CGPoint）を指定してMkMapView上の緯度経度を取得する
+            let tapPoint = sender.location(in: view)
+            let center = mapView.convert(tapPoint, toCoordinateFrom: mapView)
+
+            let lonStr = center.longitude.description
+            let latStr = center.latitude.description
+            print("lon : " + lonStr)
+            print("lat : " + latStr)
+            
+            // 現在位置とタップした位置の距離(m)を算出する
+            let distance = calcDistance(mapView.userLocation.coordinate, center)
+            print("distance : " + distance.description)
+            
+            // ロングタップを検出した位置にピンを立てる
+            pointAno.coordinate = center
+            mapView.addAnnotation(pointAno)
+            
+            // ピンに設定する文字列を生成する
+            var str:String = Int(distance).description
+            str = str + " m"
+            
+            if pointAno.title != str {
+                // ピンまでの距離に変化があればtiteを更新する
+                pointAno.title = str
+                mapView.addAnnotation(pointAno)
+            }
+        }
+    }
 }
 
